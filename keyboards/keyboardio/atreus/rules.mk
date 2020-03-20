35a2c27c3a6c5e9aaa3ebb1681efaa652581fa3e
Gergely Nagy <algernon@keyboard.io>
2020-03-20 01:51:22 +0100

    Support for the Keyboardio Atreus
    Many thanks to @noroadsleft for reviews & suggestions.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- keyboards/keyboardio/atreus/rules.mk
+++ keyboards/keyboardio/atreus/rules.mk
@@ -1,7 +1,20 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = caterina
+
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
@@ -17,6 +30,4 @@ UNICODE_ENABLE = yes        # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs
-
-DEFAULT_FOLDER = atreus/astar
+HD44780_ENABLE = no     		# Enable support for HD44780 based LCDs
